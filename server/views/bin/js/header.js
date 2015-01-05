d5d14102c837caba3bc6ca0fe256b8b17d35635b
williamking <williamjwking@gmail.com>
2015-01-05 12:53:53 +0800

    Fix some bugs

--- server/views/bin/js/header.js
+++ server/views/bin/js/header.js
@@ -1,4 +1,53 @@
 (function() {
+  var arrow_move, check, init, now_arrow;
+
+  now_arrow = 0;
+
+  init = function() {
+    switch (window.location.pathname) {
+      case '/':
+        return arrow_move(0);
+      case '/article':
+        return arrow_move(1);
+      case '/share':
+        return arrow_move(2);
+      case '/discuss':
+        return arrow_move(3);
+      case '/album':
+        return arrow_move(4);
+      case '/work':
+        return arrow_move(5);
+      case '/about':
+        return arrow_move(6);
+    }
+  };
+
+  check = function() {
+    var index, _i, _len, _ref, _results;
+    _ref = $('.link');
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      index = _ref[_i];
+      console.log("heh");
+      if (index === $('.link')[now_arrow]) {
+        $(index).removeClass("dark");
+        _results.push($(index).addClass("light"));
+      } else {
+        $(index).removeClass("light");
+        _results.push($(index).addClass("dark"));
+      }
+    }
+    return _results;
+  };
+
+  arrow_move = function(num) {
+    now_arrow = num;
+    $('.triangle').css("left", (num * 95 + 30) + "px");
+    return check();
+  };
+
+  init();
+
   $('#logout').click(function() {
     return $.ajax({
       url: '/log/session',
@@ -15,7 +64,7 @@
   $('.link').mouseover(function(event) {
     var target;
     target = $(event.currentTarget);
-    return $('.triangle').css("left", (target.attr("data-num") * 95 + 30) + "px");
+    return arrow_move(target.attr("data-num"));
   });
 
 }).call(this);
--- server/views/src/coffee/header.coffee
+++ server/views/src/coffee/header.coffee
@@ -3,6 +3,42 @@
 #	> Mail: ly.franky@gmail.com
 #	> Created Time: Thursday, November 27, 2014 AM10:15:11 CST
 
+now_arrow = 0
+
+init = ->
+    switch window.location.pathname
+        when '/'
+            arrow_move 0
+        when '/article'
+            arrow_move 1
+        when '/share' 
+            arrow_move 2
+        when '/discuss'
+           arrow_move 3
+        when '/album'
+            arrow_move 4
+        when '/work'
+            arrow_move 5
+        when '/about'
+            arrow_move 6
+
+check = ->
+    for index in $('.link')
+        if index == $('.link')[now_arrow]
+            $(index).removeClass("dark")
+            $(index).addClass("light")
+        else
+            $(index).removeClass("light")
+            $(index).addClass("dark")
+
+
+arrow_move = (num)->
+    now_arrow = num
+    $('.triangle').css "left", (num * 95 + 30) + "px"
+    check()
+
+init()
+
 $('#logout').click ->
     $.ajax
         url: '/log/session'
@@ -13,5 +49,6 @@ $('#logout').click ->
                 $(location).attr 'href', '/'
 
 $('.link').mouseover (event)->
-    target = $(event.currentTarget)
-    $('.triangle').css("left", (target.attr("data-num") * 95 + 30) + "px")
+    target = $ event.currentTarget
+    arrow_move target.attr "data-num"
+ 
